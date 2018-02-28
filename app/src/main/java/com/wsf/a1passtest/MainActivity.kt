32d1c6aa08a99b329ae68d601c42a866e85f1ade
Will <ferguson.will@gmail.com>
2018-02-28 09:57:36 -0600

    Add repo

---
+++ app/src/main/java/com/wsf/a1passtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wsf.a1passtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
